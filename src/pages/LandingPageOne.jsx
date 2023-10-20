cd02bca0e5cc41b6311c9c27b8a63a17b06ed59c
Kaustubh Patil <patilkaustubh990@gmail.com>
2023-10-20 22:21:13 +0530

    commit

--- src/pages/LandingPageOne.jsx
+++ src/pages/LandingPageOne.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 import { ChevronDown,
   //  ChevronUp, Link, 
    Menu, X } from 'lucide-react'
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const menuItems = [
   {
@@ -155,16 +156,20 @@ export function LandingPageOne() {
                 navigate('/SignUp')}
               >Join the community &rarr;</p>
             </div> */}
-            <p className="text-sm font-medium cursor-pointer"
+           
+              {/* <a href ="https://stray-aid-connect-chatrooms.vercel.app/"></a> */}
+              {/* <link rel="stylesheet" href="https://stray-aid-connect-chatrooms.vercel.app" /> */}
+              <Link to={{ pathname: "https://stray-aid-connect-chatrooms.vercel.app" }} target="_blank">
+              <p className="text-sm font-medium cursor-pointer"
               >Join the community &rarr;
-            <a href='https://stray-aid-connect-chatrooms.vercel.app/'></a>
-              </p>
+              </p></Link>
             </div>
             <h1 className="mt-8 text-3xl font-bold tracking-tight text-black md:text-4xl lg:text-6xl">
               People who care about the strays
             </h1>
             <p className="mt-8 text-lg text-gray-700">
-              A stray is a domestic animal, fowl, etc, that has wandered away and is lost. The dog was a stray which had been adopted. A stray dog or cat has wandered away from its owner&apos;s home. A stray dog came up to him.
+              {/* A stray is a domestic animal, fowl, etc, that has wandered away and is lost. The dog was a stray which had been adopted. A stray dog or cat has wandered away from its owner&apos;s home. A stray dog came up to him. */}
+              strayAidConnect is a self sustainable webApp
             </p>
             {/* <form action="" className="mt-8 flex items-start space-x-2">
                   <div>
@@ -208,8 +213,8 @@ export function LandingPageOne() {
                 stroke="currentColor"
               >
                 <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
+                  strokeLinecap="square"
+                  strokeLinejoin="square"
                   strokeWidth="1.5"
                   d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4"
                 />
