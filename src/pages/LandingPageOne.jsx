6b4a7b1460a48e022e4e19568d9f3913b2365a5f
Kaustubh Patil <patilkaustubh990@gmail.com>
2023-10-28 19:23:04 +0530

    commit

--- src/pages/LandingPageOne.jsx
+++ src/pages/LandingPageOne.jsx
@@ -32,7 +32,7 @@ const menuItems = [
 const LoadingSpinner = () => {
   return (
     <div className="flex items-center justify-center">
-      <div className="animate-spin h-6 w-6 border-t-2 border-blue-500 border-r-2 border-b-2 border-blue-200 rounded-full"></div>
+      <div className="animate-spin h-6 w-6 border-t-2 border-black border-r-2 border-b-2 border-blue-200 rounded-full"></div>
     </div>
   );
 };
@@ -108,7 +108,7 @@ export function LandingPageOne() {
         setPopupMessage('Data not submitted. Please try again.');
       }
     } catch (error) {
-      console.error('Error:', error);
+      // console.error('Error:', error);
       setPopupMessage('Data not submitted due to an error.');
     }
   };
@@ -124,20 +124,6 @@ export function LandingPageOne() {
       <header className="sticky top-0 z-50 w-full border-b bg-white pb-4 ">
         <div className="mx-auto flex max-w-7xl items-center justify-between px-1 py-4">
           <div className="inline-flex items-center space-x-2 ">
-            <span>
-              {/* <svg
-                width="40"
-                height="30"
-                viewBox="0 0 50 56"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M23.2732 0.2528C20.8078 1.18964 2.12023 12.2346 1.08477 13.3686C0 14.552 0 14.7493 0 27.7665C0 39.6496 0.0986153 41.1289 0.83823 42.0164C2.12023 43.5449 23.2239 55.4774 24.6538 55.5267C25.9358 55.576 46.1027 44.3832 48.2229 42.4602C49.3077 41.474 49.3077 41.3261 49.3077 27.8158C49.3077 14.3055 49.3077 14.1576 48.2229 13.1714C46.6451 11.7415 27.1192 0.450027 25.64 0.104874C24.9497 -0.0923538 23.9142 0.00625992 23.2732 0.2528ZM20.2161 21.8989C20.2161 22.4906 18.9835 23.8219 17.0111 25.3997C15.2361 26.7803 13.8061 27.9637 13.8061 28.0623C13.8061 28.1116 15.2361 29.0978 16.9618 30.2319C18.6876 31.3659 20.2655 32.6479 20.4134 33.0917C20.8078 34.0286 19.871 35.2119 18.8355 35.2119C17.8001 35.2119 9.0233 29.3936 8.67815 28.5061C8.333 27.6186 9.36846 26.5338 14.3485 22.885C17.6521 20.4196 18.4904 20.0252 19.2793 20.4196C19.7724 20.7155 20.2161 21.3565 20.2161 21.8989ZM25.6893 27.6679C23.4211 34.9161 23.0267 35.7543 22.1391 34.8668C21.7447 34.4723 22.1391 32.6479 23.6677 27.9637C26.2317 20.321 26.5275 19.6307 27.2671 20.3703C27.6123 20.7155 27.1685 22.7864 25.6893 27.6679ZM36.0932 23.2302C40.6788 26.2379 41.3198 27.0269 40.3337 28.1609C39.1503 29.5909 31.6555 35.2119 30.9159 35.2119C29.9298 35.2119 28.9436 33.8806 29.2394 33.0424C29.3874 32.6479 30.9652 31.218 32.7403 29.8867L35.9946 27.4706L32.5431 25.1532C30.6201 23.9205 29.0915 22.7371 29.0915 22.5892C29.0915 21.7509 30.2256 20.4196 30.9159 20.4196C31.3597 20.4196 33.6771 21.7016 36.0932 23.2302Z"
-                  fill="black"
-                />
-              </svg> */}
-            </span>
             <span className="font-bold">strayAidConnect</span>
           </div>
           <div className="hidden lg:block">
@@ -155,12 +141,6 @@ export function LandingPageOne() {
             </ul>
           </div>
           <div className="hidden lg:block">
-            {/* <button
-              type="button"
-              className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
-              onClick={() => navigate('SignUpORLogin')}
-            >Login / SingUp
-            </button> */}
           </div>
           <div className="lg:hidden">
             <Menu onClick={toggleMenu} className="h-6 w-6 cursor-pointer" />
@@ -185,7 +165,6 @@ export function LandingPageOne() {
                           />
                         </svg>
                       </span>
-                      {/* <span className="font-bold">DevUI</span> */}
                     </div>
                     <div className="-mr-2">
                       <button
@@ -213,15 +192,7 @@ export function LandingPageOne() {
                       ))}
                     </nav>
                   </div>
-                  {/* <button
-                    type="button"
-                    onClick={() => navigate('SignUpORLogin')}
-
-                    className="mt-4 w-full rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
-                  >
-                    Login / SingUp
-                  </button> */}
-                  {/* <Link to='./SingnUpLogin.jsx'></Link> */}
+                 
 
                 </div>
               </div>
@@ -237,11 +208,7 @@ export function LandingPageOne() {
               <div className="rounded-full bg-white p-1 px-2">
                 <p className="text-sm font-medium">Let&apos; make a difference</p>
               </div>
-              {/* <p className="text-sm font-medium cursor-pointer"
-              onClick={() => 
-                navigate('/SignUp')}
-              >Join the community &rarr;</p>
-            </div> */}
+           
 
               <a href="https://stray-aid-connect-chatrooms.vercel.app/" target='blank'>
                 <p className="text-sm font-medium cursor-pointer"
@@ -250,7 +217,7 @@ export function LandingPageOne() {
               </a>
             </div>
             <h1 className="mt-8 text-3xl font-bold tracking-tight text-black md:text-4xl lg:text-6xl">
-              People who care about the strays
+             For People who care about strays
             </h1>
             <p className="mt-8 text-lg text-gray-700">
               {/* A stray is a domestic animal, fowl, etc, that has wandered away and is lost. The dog was a stray which had been adopted. A stray dog or cat has wandered away from its owner&apos;s home. A stray dog came up to him. */}
@@ -305,7 +272,7 @@ export function LandingPageOne() {
                 />
               </svg>
             </div>
-            <h3 className="mt-8 text-lg font-semibold text-black">Secured Payments</h3>
+            <h3 className="mt-8 text-lg font-semibold text-black">Networks of organizations</h3>
             <p className="mt-4 text-sm text-gray-600">
               Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
               officia consequat duis enim velit mollit.
@@ -328,7 +295,7 @@ export function LandingPageOne() {
                 />
               </svg>
             </div>
-            <h3 className="mt-8 text-lg font-semibold text-black">Fast & Easy to Load</h3>
+            <h3 className="mt-8 text-lg font-semibold text-black">Accident Reports</h3>
             <p className="mt-4 text-sm text-gray-600">
               Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
               officia consequat duis enim velit mollit.
@@ -374,7 +341,7 @@ export function LandingPageOne() {
                 />
               </svg>
             </div>
-            <h3 className="mt-8 text-lg font-semibold text-black">Filter Blocks</h3>
+            <h3 className="mt-8 text-lg font-semibold text-black">Transperency</h3>
             <p className="mt-4 text-sm text-gray-600">
               Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
               officia consequat duis enim velit mollit.
@@ -467,141 +434,7 @@ export function LandingPageOne() {
           </p>
         </div>
       </section>
-      {/* Pricing Section */}
-      {/* <div className="mx-auto my-12 max-w-7xl md:my-24 lg:my-32">
-            <div className="lg:grid lg:grid-cols-12 lg:gap-x-6">
-              <div className="px-4 py-10 lg:col-span-5 lg:px-0">
-                <span className="mb-8 inline-block rounded-full border p-1 px-3 text-xs font-semibold">
-                  Pricing that fits your budget
-                </span>
-                <h1 className="text-3xl font-bold md:text-5xl">
-                  Lorem ipsum dolor sit amet consectetur.
-                </h1>
-                <p className="mt-8 font-medium">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam magni, rem sed
-                  sint neque doloribus saepe veniam minima quaerat minus.
-                </p>
-                <div className="mt-8 flex w-full max-w-sm items-center space-x-2">
-                  <input
-                    className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-                    type="email"
-                    placeholder="Email"
-                  ></input>
-                  <button
-                    type="button"
-                    className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
-                  >
-                    Subscribe
-                  </button>
-                </div>
-              </div>
-              <div className="flex flex-col items-center justify-center md:flex-row lg:col-span-7">
-                <div className="w-full p-5 md:w-1/2">
-                  <div className="rounded-md border bg-white bg-opacity-90">
-                    <div className=" border-b">
-                      <div className="px-9 py-7">
-                        <h3 className="mb-3 text-xl font-bold leading-snug text-gray-900">Standard</h3>
-                        <p className="font-medium leading-relaxed text-gray-500">
-                          Lorem ipsum dolor sit amet, consect etur adipiscing maror.
-                        </p>
-                      </div>
-                    </div>
-                    <div className="px-9 pb-9 pt-8">
-                      <p className="mb-6 font-medium leading-relaxed text-gray-600">
-                        Features included:
-                      </p>
-                      <ul className="mb-11">
-                        <li className="mb-4 flex items-center">
-                          <CheckCircle className="mr-2" size={16} />
-                          <p className="font-semibold leading-normal">3 Team Members</p>
-                        </li>
-                        <li className="mb-4 flex items-center">
-                          <CheckCircle className="mr-2" size={16} />
-                          <p className="font-semibold leading-normal">1200+ UI Blocks</p>
-                        </li>
-                        <li className="mb-4 flex items-center">
-                          <CheckCircle className="mr-2" size={16} />
-                          <p className="font-semibold leading-normal">10 GB Cloud Storage</p>
-                        </li>
-                        <li className="mb-4 flex items-center">
-                          <CheckCircle className="mr-2" size={16} />
-                          <p className="font-semibold leading-normal">Individual Email Account</p>
-                        </li>
-                        <li className="flex items-center">
-                          <CheckCircle className="mr-2" size={16} />
-                          <p className="font-semibold leading-normal">Premium Support</p>
-                        </li>
-                      </ul>
-                      <p className="mb-6 text-lg font-semibold leading-normal text-gray-600">
-                        <span>Starting from</span>
-                        <span className="ml-2 text-gray-900">$49/mo</span>
-                      </p>
-                      <div className="md:inline-block">
-                        <button
-                          type="button"
-                          className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
-                        >
-                          Start your free trial
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="w-full p-5 md:w-1/2">
-                  <div className="rounded-md border bg-white bg-opacity-90">
-                    <div className=" border-b">
-                      <div className="px-9 py-7">
-                        <h3 className="mb-3 text-xl font-bold leading-snug text-gray-900">Standard</h3>
-                        <p className="font-medium leading-relaxed text-gray-500">
-                          Lorem ipsum dolor sit amet, consect etur adipiscing maror.
-                        </p>
-                      </div>
-                    </div>
-                    <div className="px-9 pb-9 pt-8">
-                      <p className="mb-6 font-medium leading-relaxed text-gray-600">
-                        Features included:
-                      </p>
-                      <ul className="mb-11">
-                        <li className="mb-4 flex items-center">
-                          <CheckCircle className="mr-2" size={16} />
-                          <p className="font-semibold leading-normal">3 Team Members</p>
-                        </li>
-                        <li className="mb-4 flex items-center">
-                          <CheckCircle className="mr-2" size={16} />
-                          <p className="font-semibold leading-normal">1200+ UI Blocks</p>
-                        </li>
-                        <li className="mb-4 flex items-center">
-                          <CheckCircle className="mr-2" size={16} />
-                          <p className="font-semibold leading-normal">10 GB Cloud Storage</p>
-                        </li>
-                        <li className="mb-4 flex items-center">
-                          <CheckCircle className="mr-2" size={16} />
-                          <p className="font-semibold leading-normal">Individual Email Account</p>
-                        </li>
-                        <li className="flex items-center">
-                          <CheckCircle className="mr-2" size={16} />
-                          <p className="font-semibold leading-normal">Premium Support</p>
-                        </li>
-                      </ul>
-                      <p className="mb-6 text-lg font-semibold leading-normal text-gray-600">
-                        <span>Starting from</span>
-                        <span className="ml-2 text-gray-900">$49/mo</span>
-                      </p>
-                      <div className="md:inline-block">
-                        <button
-                          type="button"
-                          className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
-                        >
-                          Start your free trial
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div> */}
-
+     
       {/* NewsLetter */}
       {/* <div className="mx-auto max-w-7xl bg-gray-50 px-2 py-10 lg:px-2">
             <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between">
@@ -650,10 +483,10 @@ export function LandingPageOne() {
                 </p>
               </div>
             </div>
-          </div> */}
+          </div> 
 
-      {/* footer */}
-      {/* <div className="mx-auto mt-12 max-w-7xl">
+       {/* footer  */}
+       {/* <div className="mx-auto mt-12 max-w-7xl">
             <footer className="px-4 py-10">
               <div className="flex flex-col md:flex-row md:items-center">
                 <span>
@@ -728,7 +561,7 @@ export function LandingPageOne() {
                   </ul>
                 </div>
               </div>
-            </footer> */}
+            </footer>  */}
       {/* </div> */}
       <div className="mx-auto max-w-7xl px-4">
         {/* Hero Map */}
